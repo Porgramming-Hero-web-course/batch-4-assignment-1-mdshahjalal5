c20793d168771b9c66f7877c659689bd2484af5f
Md. Shahjalal Sheikh Bin Karsk <muhommodshahjalal@gmail.com>
2024-11-07 09:22:33 +0600

    main task started

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// src/index.ts
-console.log("Hello, TypeScript with pnpm!");
-//w: 06/11/2024 10:18 PM Wed GMT+6 Sharifpur, Gazipur, Dhaka
-console.log(`ami ts projects doing mere dost`);
-console.log(`ami ts projects doing mere dost`);

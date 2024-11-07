7b44601ea4eb1cbdc49e93d490b9234b825725ad
Md. Shahjalal Sheikh Bin Karsk <muhommodshahjalal@gmail.com>
2024-11-07 14:11:03 +0600

    problem 5 solved

--- src/problem-5.ts
+++ src/problem-5.ts
@@ -1,2 +1,10 @@
 //w: 07/11/2024 01:59 PM Thu GMT+6 Sharifpur, Gazipur, Dhaka
-//problem-4.ts
+//problem-5.ts
+//
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const person = { name: "sj", age: 20 };
+console.log(getProperty(person, "name"));
+console.log(getProperty(person, "age"));

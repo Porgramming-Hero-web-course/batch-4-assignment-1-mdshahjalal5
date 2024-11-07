ded886e736f0723ea8fde6922d27bf3662ab390e
Md. Shahjalal Sheikh Bin Karsk <muhommodshahjalal@gmail.com>
2024-11-07 10:45:45 +0600

    problem 2 solved

--- dist/problem-2.js
+++ dist/problem-2.js
@@ -1,2 +1,9 @@
 "use strict";
 //w: 07/11/2024 10:02 AM Thu GMT+6 Sharifpur, Gazipur, Dhaka
+//
+// problem-1.ts
+function removeDuplicates(nums) {
+    return [...new Set(nums)];
+}
+const res = removeDuplicates([3, 4, 5, 6, 3, 5, 5, 6, 6]);
+console.log(res); // Output: [3, 4, 5, 6]
--- src/problem-2.ts
+++ src/problem-2.ts
@@ -1,1 +1,9 @@
 //w: 07/11/2024 10:02 AM Thu GMT+6 Sharifpur, Gazipur, Dhaka
+//
+// problem-1.ts
+function removeDuplicates(nums: number[]): number[] {
+  return [...new Set(nums)];
+}
+
+const res = removeDuplicates([3, 4, 5, 6, 3, 5, 5, 6, 6]);
+console.log(res); // Output: [3, 4, 5, 6]

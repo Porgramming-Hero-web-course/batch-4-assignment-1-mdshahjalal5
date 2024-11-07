7d78a0895871aee56abdfd2306fe275c476e4658
Md. Shahjalal Sheikh Bin Karsk <muhommodshahjalal@gmail.com>
2024-11-07 14:12:35 +0600

    problem 5 modified

--- src/problem-5.ts
+++ src/problem-5.ts
@@ -1,7 +1,7 @@
 //w: 07/11/2024 01:59 PM Thu GMT+6 Sharifpur, Gazipur, Dhaka
 //problem-5.ts
 //
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+function getProperty<O, P extends keyof O>(obj: O, key: P): O[P] {
   return obj[key];
 }
 
